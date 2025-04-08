7c2673c830a622b7c6912b397f1486df708102b3
Melda <david@fml.cl>
2025-04-08 20:58:58 +0200

    Lambda function test

--- lib/lupworlds-cdk-stack.ts
+++ lib/lupworlds-cdk-stack.ts
@@ -1,11 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class LupworldsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Defines Lambda function resource
+    const handleUserRequest = new lambda.Function(this, 'HandleUserRequest', {
+      code: lambda.Code.fromAsset('src/user'),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_22_X,
+    });
+
+    const userHandlerUrl = handleUserRequest.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+
+    new cdk.CfnOutput(this, 'userHandlerUrl', {
+      value: userHandlerUrl.url,
+    })
     // The code that defines your stack goes here
 
     // example resource

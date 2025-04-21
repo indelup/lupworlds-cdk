b7cbdb94b3a21e15d92d6f53c8a1935af695bae7
Vani <linda@fml.cl>
2025-04-21 00:14:38 -0400

    feat: add user API with dynamoDB and OpenAPI Gateway

--- lib/lupworlds-cdk-stack.ts
+++ lib/lupworlds-cdk-stack.ts
@@ -2,30 +2,67 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import { OpenApiGatewayToLambda } from '@aws-solutions-constructs/aws-openapigateway-lambda';
+import { LambdaToDynamoDB } from '@aws-solutions-constructs/aws-lambda-dynamodb';
+import { Asset } from 'aws-cdk-lib/aws-s3-assets';
+import * as path from 'path';
+import * as ddb from 'aws-cdk-lib/aws-dynamodb';
 
 export class LupworldsCdkStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        // Defines Lambda function resource
-        const handleUserRequest = new lambda.Function(
-            this,
-            "HandleUserRequest",
-            {
-                code: lambda.Code.fromAsset("src/user"),
-                handler: "index.handler",
-                runtime: lambda.Runtime.NODEJS_22_X,
+        const simpleTableProps = {
+            partitionKey: {
+                name: "id",
+                type: ddb.AttributeType.STRING,
             },
-        );
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+        }
 
-        const userHandlerUrl = handleUserRequest.addFunctionUrl({
-            authType: lambda.FunctionUrlAuthType.NONE,
+        const userApparatus = new LambdaToDynamoDB(this, "UserApparatus", {
+            lambdaFunctionProps: {
+                runtime: lambda.Runtime.NODEJS_22_X,
+                handler: "index.handler",
+                code: lambda.Code.fromAsset("src/user"),
+            },
+            dynamoTableProps: simpleTableProps,
         });
 
-        new cdk.CfnOutput(this, "userHandlerUrl", {
-            value: userHandlerUrl.url,
+        const userApi = new OpenApiGatewayToLambda(this, "UserOpenApiGatewayToLambda", {
+            apiDefinitionAsset: new Asset(this, 'ApiDefinitionAsset', {
+                path: path.join("openapi", "users-api.yaml"),
+            }),
+            apiIntegrations: [
+                {
+                    id: "UserHandler",
+                    existingLambdaObj: userApparatus.lambdaFunction
+                }
+            ]
         });
-        // The code that defines your stack goes here
+
+        new cdk.CfnOutput(this, "UserUrl", {
+            value: userApi.apiGateway.url + "users",
+        });      
+
+        // Defines Lambda function resource
+        //const handleUserRequest = new lambda.Function(
+        //    this,
+        //    "HandleUserRequest",
+        //    {
+        //        code: lambda.Code.fromAsset("src/user"),
+        //        handler: "index.handler",
+        //        runtime: lambda.Runtime.NODEJS_22_X,
+        //    },
+        //);
+
+        //const userHandlerUrl = handleUserRequest.addFunctionUrl({
+        //    authType: lambda.FunctionUrlAuthType.NONE,
+        //});
+
+        //  new cdk.CfnOutput(this, "userHandlerUrl", {
+        //    value: userHandlerUrl.url,
+        //});
 
         // example resource
         // const queue = new sqs.Queue(this, 'LupworldsCdkQueue', {
--- src/user/index.ts
+++ src/user/index.ts
@@ -1,6 +1,130 @@
-exports.handler = async (_event: any) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify("Hello from Lambda!"),
-    };
+//exports.handler = async (_event: any) => {
+//    return {
+//        statusCode: 200,
+//        body: JSON.stringify("Hello from Lambda!"),
+//    };
+//};
+
+// User Handler
+const ddb = require('@aws-sdk/client-dynamodb');
+const client = new ddb.DynamoDBClient({ region: process.env.AWS_REGION });
+
+// Get the table name from the Environment Variable set by aws-lambda-dynamodb
+const userTableName = process.env.DDB_TABLE_NAME;
+
+type User = {
+    id: string;
+    twitchId: string;
+    alias: string;
+    allowedRoles: string[];
+    worldIds: string[];
+}
+
+exports.handler = async (event: any) => {
+    let result: any;
+
+    try {
+        console.log('Event:', JSON.stringify(event));
+        // Normalize path by getting the last segment
+        const normalizedPath = '/' + event.path.split('/').filter(Boolean).pop();
+        console.log('Normalized path:', normalizedPath);
+
+        if (event.httpMethod === 'POST' && normalizedPath === '/users') {
+            const body = JSON.parse(event.body);
+            console.log('Parsed body:', JSON.stringify(body));
+            const newId = await createUser(body);
+            result = { id: newId };
+        } else if (event.httpMethod === 'GET' && normalizedPath === '/users') {
+            result = await listUsers();
+        } else if (event.httpMethod === 'GET' && event.pathParameters?.userId) {
+            result = await getUser(event.pathParameters.userId);
+        } else {
+            console.log(`Unsupported method: ${event.httpMethod} path: ${normalizedPath}`);
+            throw { statusCode: 405 };
+        }
+    } catch (e: any) {
+        console.error('Error:', e);
+        return createReturnObject(e.statusCode || 500);
+    }
+
+    return createReturnObject(200, JSON.stringify(result));
 };
+
+async function getUser(userId: string): Promise<User> {
+    try {
+        const command = new ddb.GetItemCommand({
+            TableName: userTableName,
+            Key: {
+                "id": { S: userId }
+            }
+        });
+
+        const data = await client.send(command);
+        if (!data.Item) {
+            throw { statusCode: 404 };
+        }
+
+        return {
+            id: data.Item.id.S,
+            twitchId: data.Item.twitchId.S,
+            alias: data.Item.alias.S,
+            allowedRoles: data.Item.allowedRoles.SS,
+            worldIds: data.Item.worldIds.SS
+        };
+    } catch (e) {
+        console.log(`Failed Dynamodb processing: ${JSON.stringify(e)}`);
+        throw { statusCode: 500 };
+    }
+}
+
+async function listUsers(): Promise<User[]> {
+    try {
+        const command = new ddb.ScanCommand({
+            TableName: userTableName
+        });
+
+        const data = await client.send(command);
+        return (data.Items || []).map((item: any) => ({
+            id: item.id.S,
+            twitchId: item.twitchId.S,
+            alias: item.alias.S,
+            allowedRoles: item.allowedRoles.SS,
+            worldIds: item.worldIds.SS
+        }));
+    } catch (e) {
+        console.log(`Failed Dynamodb processing: ${JSON.stringify(e)}`);
+        throw { statusCode: 500 };
+    }
+}
+
+async function createUser(user: Omit<User, 'id'>): Promise<string> {
+    try {
+        const newId = `usr${Date.now()}`;
+        const command = new ddb.PutItemCommand({
+            TableName: userTableName,
+            Item: {
+                'id': { S: newId },
+                'twitchId': { S: user.twitchId },
+                'alias': { S: user.alias },
+                'allowedRoles': { SS: user.allowedRoles },
+                'worldIds': { SS: user.worldIds?.length > 0 ? user.worldIds : [''] }
+            }
+        });
+
+        await client.send(command);
+        return newId;
+    } catch (e) {
+        console.error('Failed DynamoDB processing:', JSON.stringify(e));
+        throw { statusCode: 500 };
+    }
+}
+
+function createReturnObject(httpStatusCode: number, body?: string) {
+    console.log(httpStatusCode);
+    console.log(JSON.stringify(body));
+    return {
+        statusCode: httpStatusCode,
+        headers: { "Content-Type": "application/json" },
+        body: httpStatusCode === 200 ? body : undefined,
+    };
+}

43949c2eb042562aaf42ac7b2650ca77407d5b85
Melda <david@fml.cl>
2025-06-24 22:23:29 +0200

    Added CORS to api

--- apiProxyLambda/index.ts
+++ apiProxyLambda/index.ts
@@ -1,9 +1,22 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { handle } from "hono/aws-lambda";
 import characters from "./resources/characters";
 import users from "./resources/users";
 
 const app = new Hono();
+
+app.use(
+    "/*",
+    cors({
+        origin: "http://localhost:8080",
+        allowMethods: ["GET", "POST", "PUT", "DELETE"],
+        allowHeaders: ["Content-Type", "Authorization"],
+        exposeHeaders: ["Content-Length"],
+        maxAge: 3600,
+        credentials: true,
+    }),
+);
 
 app.route("/characters", characters);
 app.route("/users", users);
